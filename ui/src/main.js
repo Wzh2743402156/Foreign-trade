66a3816d3987666b57d701073c8f4af10a76c497
Harry <zhihanwang2001@outlook.com>
2025-04-05 16:10:55 +0800

    rebuild

--- ui/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import VChart from 'vue-echarts'
-import 'element-plus/theme-chalk/el-message.css'
-import { ElMessage } from 'element-plus'
-
-
-createApp(App).use(router).mount('#app')
